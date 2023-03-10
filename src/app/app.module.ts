1cb0a3a7cdb0e84d94c6abbf89c77544e51632f4
Max Butler <maxgbutler003@gmail.com>
2023-03-09 19:57:29 -0600

    new header, crew, experiments, equiptment components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CrewComponent } from './crew/crew.component';
+import { EquiptmentComponent } from './equiptment/equiptment.component';
+import { ExperimentsComponent } from './experiments/experiments.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CrewComponent,
+    EquiptmentComponent,
+    ExperimentsComponent
   ],
   imports: [
     BrowserModule
